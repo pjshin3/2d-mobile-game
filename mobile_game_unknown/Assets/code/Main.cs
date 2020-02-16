43c27d8dbf4bf32c1a8c58f22a3a9af80b218aac
pjshin3 <tlsvlfwp1@naver.com>
2020-02-16 17:22:26 +0900

    6 디자인
    백그라운드 이미지
    - 나무 무덤

--- mobile_game_unknown/Assets/code/Main.cs
+++ mobile_game_unknown/Assets/code/Main.cs
@@ -46,13 +46,13 @@ public class Main : MonoBehaviour
 
     void move()
     {
-
         if (!inputleft && !inputright)
         {
             animator.SetBool("isrunning", false);
         }
         else if (inputleft)
         {
+           
             transform.localScale = new Vector3(-1, 1, 1);
             moveVelocity = Vector3.left;
             animator.SetBool("isrunning", true);
@@ -61,6 +61,7 @@ public class Main : MonoBehaviour
         }
         else if (inputright)
         {
+            
             transform.localScale = new Vector3(1, 1, 1);
             moveVelocity = Vector3.right;
             animator.SetBool("isrunning", true);
@@ -76,24 +77,8 @@ public class Main : MonoBehaviour
             inpuAtteck = false;
             animator.SetTrigger("atteck");
 
-            //if (transform.localScale.x == -1)
-            //{
-            //    GameObject werter = Instantiate(Werter, new Vector3(transform.position.x - 4, transform.position.y, 49), Quaternion.identity);
-            //    werter.GetComponent<Werter>().init();
-            //    werter.GetComponent<Werter>().isclone = true;
-
-            //    Destroy(werter, 0.4f);
-            //}
-            //else
-            //{
-            //    GameObject werter = Instantiate(Werter, new Vector3(transform.position.x + 4, transform.position.y, 49), Quaternion.identity);
-            //    werter.GetComponent<Werter>().isclone = true;
-            //    werter.GetComponent<Werter>().init();
-            //    werter.GetComponent<Werter>().input_darection = true;
-
-            //    Destroy(werter, 0.4f);
-            //}
-
+            this.gameObject.tag = "PlayerAtteck";
         }
+        
     }
 }
---
+++ mobile_game_unknown/Assets/code/enermy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enermy : MonoBehaviour
+{
+
+    GameObject effectC;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        effectC = Resources.Load<GameObject>("effect/effect_c_02");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if(collision.gameObject.tag == "PlayerAtteck")
+        {
+            if (transform.localScale.x == -1)
+            {
+                GameObject effect = Instantiate(effectC, new Vector3(transform.position.x -0.5f, transform.position.y, -2), Quaternion.identity);
+                Destroy(effect, 0.4f);
+            }
+            else
+            {
+                GameObject effect = Instantiate(effectC, new Vector3(transform.position.x - 0.5f, transform.position.y, -2), Quaternion.identity);
+                Destroy(effect, 0.4f);
+            }
+        }
+    }
+}
